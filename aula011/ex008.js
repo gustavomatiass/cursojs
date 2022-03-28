f874ff126f659bbda619a33720bdf3cec803dac6
gustavomatiass <gustavomatiass123@gmail.com>
2022-03-28 11:31:10 -0300

    aula 11

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20.5
+console.log(`A velocidade do seu carro é  ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Voce passou da velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre com o cinto de segurança`)
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país} `)
+if (país == 'Brasil') { 
+    console.log('Vc é Brasileiro')
+}
+else {
+    console.log('Vc é Estrangeiro')
+}
