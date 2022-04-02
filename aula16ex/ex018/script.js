904d9bce1373074a09568a094ff45da82f2bc99a
gustavomatiass <gustavomatiass123@gmail.com>
2022-04-02 11:57:00 -0300

    curso finalizado

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,28 +1,60 @@
-let num = document.getElementById('fnum')
-let flista = document.getElementById('flista')
-let res = document.getElementById('res')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
 
-function isNumero(n){
-    if (Number(n) >= 1 && Number(n) <= 100) {
+function isnumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100 ){
         return true
     }else {
         return false
     }
 }
 
-function inList(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-      return true;
-    } else {
-      return false
- }
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else {
+        return false
+    }
 }
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('opa')
+function adicionar(){
+    if(isnumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else{
+        window.alert('Valor invalido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0) {
+        window.alert('Adiciona valores antes de finalizar')
     }else {
-        window.alert('Valor invalido ou já encontrado na lista')
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} numeros cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado doi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores temo ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${media}</p>`
     }
 }
